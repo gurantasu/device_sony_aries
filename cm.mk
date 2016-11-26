cc41773d286480da7530d7bd8d4ac8eb18009020
gurantasu <mtlhn.mslc@gmail.com>
2016-11-26 19:31:30 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -11,9 +11,6 @@ $(call inherit-product, device/sony/shinano/platform2.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Fingerprint for aries (from stock)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:5.1.1/23.4.A.0.546/864948651:user/release-keys
